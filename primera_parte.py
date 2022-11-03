50b6bbccd4b0bfb52f4eecb807fb6f19207309f4
JhohanJC <111875@unsaac.edu.pe>
2022-11-02 20:42:05 -0500

    Funcion agregar_libro

--- primera_parte.py
+++ primera_parte.py
@@ -13,3 +13,18 @@ def listar_libros():
     print("Listado de libros")
     for v,a in enumerate(lista_libros):
         print(f"{v} -> {a.titulo}, {a.genero}, {a.id_ISBN}, {a.editorial}, {a.autores}")
+def agregar_libro():
+    print("Agregar libro")
+    print("Ingrese los siguientes datos del libro: ")
+    nm=Libro()
+    nm.titulo=input("Título del libro: ")
+    nm.genero=input("Género del libro: ")
+    nm.id_ISBN=input("ID o ISBN: ")
+    nm.editorial=input("Editorial: ")
+    nmk=int(input("Ingrese la cantidad de autores: "))
+    autor=[]
+    for vk in range(nmk):
+        a=input(f"Ingrese el autor {vk+1}: ")
+        autor.append(a)
+    nm.autores=autor
+    lista_libros.append(nm)
