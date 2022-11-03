c3ca090db1e3c9e2697813d6ce1fb13776722773
JhohanJC <111875@unsaac.edu.pe>
2022-11-02 20:31:41 -0500

    funcion listar_libros

--- primera_parte.py
+++ primera_parte.py
@@ -7,3 +7,9 @@ class Libro:
         self.id_ISBN=""
         self.editorial=""
         self.autores=""
+def leer_archivo():
+    print("Leer archivos CSV o txt")
+def listar_libros():
+    print("Listado de libros")
+    for v,a in enumerate(lista_libros):
+        print(f"{v} -> {a.titulo}, {a.genero}, {a.id_ISBN}, {a.editorial}, {a.autores}")
